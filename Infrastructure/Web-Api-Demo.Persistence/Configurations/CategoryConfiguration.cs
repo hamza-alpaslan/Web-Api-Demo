62f9dae12efe1e165ee6f587e1b11db85314f24f
hamza alpaslan <hamza250alp@gmail.com>
2025-02-13 18:46:14 +0300

    Adding Configurations

---
+++ Infrastructure/Web-Api-Demo.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web_Api_Demo.Domain.Common.Entities;
+
+namespace Web_Api_Demo.Persistence.Configurations
+{
+    internal class CategoryConfiguration:IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            Category category1 = new Category() { Id = 1, Name = "Elektrik", Priorty = 1, ParentId = 0, IsDeleted = false, CreateDate = DateTime.Now };
+            Category category2 = new Category() { Id = 2, Name = "Moda", Priorty = 1, ParentId = 0, IsDeleted = false, CreateDate = DateTime.Now };
+            Category category3 = new Category() { Id = 3, Name = "Bilgisayar", Priorty = 1, ParentId = 1, IsDeleted = false, CreateDate = DateTime.Now };
+            Category category4 = new Category() { Id = 4, Name = "Kadın", Priorty = 1, ParentId = 2, IsDeleted = false, CreateDate = DateTime.Now };
+            builder.HasData(category1, category2, category3, category4);
+        }
+    }
+}
---
+++ Infrastructure/Web-Api-Demo.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web_Api_Demo.Domain.Common;
+using Web_Api_Demo.Domain.Common.Entities;
+
+namespace Web_Api_Demo.Persistence.Configurations
+{
+    internal class DetailConfiguration:IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            Detail detail1 = new Detail() {Id=1,Title="Elektrik Ozellikleri",Description="Açıklama",CategoryId=1,CreateDate=DateTime.Now,IsDeleted=false };
+            Detail detail2 = new Detail() { Id =2, Title = "Bilgisayar Özellikleri", Description = "Açıklama", CategoryId = 3, CreateDate = DateTime.Now, IsDeleted = true };
+            Detail detail3 = new Detail() { Id = 3, Title = "Kadın Moda Ozellikleri", Description = "Açıklama", CategoryId = 4, CreateDate = DateTime.Now, IsDeleted = false };
+            builder.HasData(detail1,detail2,detail3);
+        }
+    }
+}
